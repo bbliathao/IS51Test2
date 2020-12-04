2c7e80e96b3c9e5188c056560150b8aa62383925
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-03 17:39:16 -0800

    exam end

--- test2.py
+++ test2.py
@@ -6,3 +6,18 @@ then a calculate percent above average funtion to
 calculate the percentage of garades that are above average grade. 
 """
 
+"""
+to find the average:
+Calculate and report the average grade of the class.
+Initialize counter and sum to 0
+while there are more data
+    get the next grade
+    increment the counter
+    add the grade to the sum
+set average to sum / counter
+display average
+to find the percentage:
+    total of scores * 2 / #'s of student
+    
+"""
+
