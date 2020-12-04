6c562056a222a8dec0683370c4897c03c3792a1c
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-03 16:33:07 -0800

    finished laymens

---
+++ test2.py
@@ -0,0 +1,8 @@
+"""
+The program is trying to return the number of grades, 
+average grades, and percentage of grades. 
+First we will write a main function to initialse the application
+then a calculate percent above average funtion to 
+calculate the percentage of garades that are above average grade. 
+"""
+
