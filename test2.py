4a88d9d67938067b92b559a497b405436aed5433
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-03 17:35:38 -0800

    finished pseudocode

--- test2.py
+++ test2.py
@@ -1,8 +1,25 @@
 """
 The program is trying to return the number of grades, 
 average grades, and percentage of grades. 
-First we will write a main function to initialse the application
-then a calculate percent above average funtion to 
-calculate the percentage of garades that are above average grade. 
+First we will write a main function to initialize the application
+to determine the average grade
+then a calculate percent above average function to 
+calculate the percentage of grades that are above average grade. 
+"""
+
+"""
+to find the average:
+
+Calculate and report the average grade of the class.
+Initialize counter and sum to 0
+while there are more data
+    get the next grade
+    increment the counter
+    add the grade to the sum
+set average to sum / counter
+display average
+
+to find the percentage:
+    total of scores * 2 / #'s of student
 """
 
